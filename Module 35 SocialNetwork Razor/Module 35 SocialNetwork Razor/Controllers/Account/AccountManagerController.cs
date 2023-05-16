8ec8e9b75eaaff2b7017703e78a2e465a04e9ca1
danii502 <danii50283@edu.taitotalo.fi>
2023-05-16 12:31:23 +0300

    Innit commit

--- Module 35 SocialNetwork Razor/Module 35 SocialNetwork Razor/Controllers/Account/AccountManagerController.cs
+++ Module 35 SocialNetwork Razor/Module 35 SocialNetwork Razor/Controllers/Account/AccountManagerController.cs
@@ -30,8 +30,28 @@ namespace Module_35_SocialNetwork_Razor.Controllers.Account
             _signInManager = signInManager;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+
         }
 
+
+        [Route("Generate")]
+        [HttpGet]
+        public async Task<IActionResult> Generate()
+        {
+
+            var usergen = new GenetateUsers();
+            var userlist = usergen.Populate(35);
+
+            foreach (var user in userlist)
+            {
+                var result = await _userManager.CreateAsync(user, "123456");
+
+                if (!result.Succeeded)
+                    continue;
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
 
         [Route("Login")]
         [HttpGet]
@@ -223,6 +243,71 @@ namespace Module_35_SocialNetwork_Razor.Controllers.Account
             };
 
             return model;
+        }
+
+
+        [Route("Chat")]
+        [HttpPost]
+        public async Task<IActionResult> Chat(string id)
+        {
+            var model = await GenerateChat(id);
+            return View("Chat", model);
+        }
+
+        private async Task<ChatViewModel> GenerateChat(string id)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+            var friend = await _userManager.FindByIdAsync(id);
+
+
+            var repository = _unitOfWork.GetRepository<Message>() as MessageRepository;
+
+            var mess = repository.GetMessages(result, friend);
+
+            var model = new ChatViewModel()
+            {
+                You = result,
+                ToWhom = friend,
+                History = mess.OrderBy(x => x.Id).ToList(),
+            };
+
+            return model;
+        }
+
+        [Route("Chat")]
+        [HttpGet]
+        public async Task<IActionResult> Chat()
+        {
+
+            var id = Request.Query["id"];
+
+            var model = await GenerateChat(id);
+            return View("Chat", model);
+        }
+
+        [Route("NewMessage")]
+        [HttpPost]
+        public async Task<IActionResult> NewMessage(string id, ChatViewModel chat)
+        {
+            var currentuser = User;
+
+            var result = await _userManager.GetUserAsync(currentuser);
+            var friend = await _userManager.FindByIdAsync(id);
+
+            var repository = _unitOfWork.GetRepository<Message>() as MessageRepository;
+
+            var item = new Message()
+            {
+                Sender = result,
+                Recipient = friend,
+                Text = chat.NewMessage.Text,
+            };
+            repository.Create(item);
+
+            var model = await GenerateChat(id);
+            return View("Chat", model);
         }
     }
 }
